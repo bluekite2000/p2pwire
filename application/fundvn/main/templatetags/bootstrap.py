bf40ce2700adb95ac0c08add0b85cbb715eb7019
dat nguyen <nguyenhdat@gmail.com>
2011-08-24 18:13:22 -0400

    change transfer table to form

--- application/fundvn/main/templatetags/bootstrap.py
+++ application/fundvn/main/templatetags/bootstrap.py
@@ -5,11 +5,17 @@ from django import template
 register = template.Library()
 
 @register.filter
-def bootstrap(form):
-    template = get_template("bootstrap_form/form.html")
+def bootstrap_table(form):
+    template = get_template("bootstrap/table.html")
     context = Context({'form': form})
     return template.render(context)
 
+
+@register.filter
+def bootstrap_form(form):
+    template = get_template("bootstrap/form.html")
+    context = Context({'form': form})
+    return template.render(context)
 @register.filter
 def is_checkbox(field):
     return field.field.widget.__class__.__name__.lower() == "checkboxinput"
